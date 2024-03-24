d9d20734d6fd045b216b6bace9a010873a53a660
Mehul <lorditachi79@gmail.com>
2024-03-25 01:13:39 +0530

    use of interfaces and default methods

---
+++ src/com/company/Java_57_Interfaces_and_defaultMethods.java
@@ -0,0 +1,58 @@
+package com.company;
+interface Camera{
+    void snapshot();
+    void recordVideo();
+}
+interface Wifi{
+    String[] getAvailableNetworks();
+    void connectNetworks(String network);
+    void disconnect();
+}
+class Phone{
+    void callNumber(int number){
+        System.out.println("Calling ... "+ number);
+    }
+    void pickCall(){
+        System.out.println("Connecting ... ");
+    }
+}
+class SmartPhone extends Phone implements Camera , Wifi{
+    String connectedWifi;
+    public void snapshot(){
+        System.out.println("Taking snapshot !!! ");
+    }
+    public void recordVideo(){
+        System.out.println("Recording Video !!! ");
+    }
+    public String [] getAvailableNetworks(){
+        String [] availabeNetworks = {"Home","office1","Neighbour","Public wifi",};
+        return availabeNetworks;
+    }
+    public void connectNetworks(String network) {
+        this.connectedWifi=network;
+    }
+    public void disconnect(){
+        this.connectedWifi=null;
+    }
+
+    @Override
+    void pickCall() {
+        String line = "Secured Connection establishing ...";
+        System.out.println(line);
+    }
+}
+public class Java_57_Interfaces_and_defaultMethods {
+    public static void main(String[] args) {
+        SmartPhone myPhone = new SmartPhone();
+        for (String network : myPhone.getAvailableNetworks()){
+            if (network.equals("Home")){
+                myPhone.connectNetworks(network);
+            }
+        }
+        System.out.println("You are connected to the Wifi : "+myPhone.connectedWifi);
+        myPhone.callNumber(123456789);
+        myPhone.recordVideo();
+
+
+    }
+}
